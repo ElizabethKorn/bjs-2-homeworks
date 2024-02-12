3be681f4cccdda19a968b1e4c562d3210cdc7928
Liza <lkorn036@gmail.com>
2024-02-12 21:57:26 +0300

    Исправлены все ошибки во втором задании

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,12 +1,12 @@
 function getArrayParams(...arr) {
   let max = arr[0];
   let min = arr[0];
-  let sum = 0;
+  let sum = 0, avg;
   for (let i = 0; i < arr.length; i++) {
-    max = Math.max(...arr);
-    min = Math.min(...arr);
     sum += arr[i]; 
   }
+  max = Math.max(...arr);
+  min = Math.min(...arr);
   avg = sum / arr.length;
   avg = Number(parseFloat(avg.toFixed(2)));
   return { min: min, max: max, avg: avg };
@@ -14,9 +14,6 @@ function getArrayParams(...arr) {
 
 function summElementsWorker(...arr) {
   let sum = 0;
-  if (arr.length === 0) {
-      return 0;
-  }
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
@@ -29,14 +26,11 @@ function differenceMaxMinWorker(...arr) {
   }
   const max = Math.max(...arr);
   const min = Math.min(...arr);
-  return (max - min);
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
   let sumEvenElement = 0, sumOddElement = 0;
-  if (arr.length === 0) {
-      return 0;
-  }
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
       sumEvenElement+=arr[i];
@@ -45,7 +39,7 @@ function differenceEvenOddWorker(...arr) {
       sumOddElement+=arr[i];
     }
   }
-  return (sumEvenElement - sumOddElement);
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
@@ -59,7 +53,7 @@ function averageEvenElementsWorker(...arr) {
       countEvenElement++;
     }
   }
-  return (sumEvenElement / countEvenElement);
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
